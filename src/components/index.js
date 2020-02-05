17127007fcb8d168c27c01b3fd1e8af241e96481
jordan-oos <jordan@oos-studio.com>
2020-02-05 13:57:57 -0500

    Removed imports for other components from index.js.

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,3 @@
-import Footer from './Footer'
 import CardGrid from './CardGrid'
-import FullScreenIntro from './FullScreenIntro'
 
-export { Footer }
 export { CardGrid }
-export { FullScreenIntro }
