1c7f3f9d6eb7ccc072a2378d28cd63113a9f1fb7
jordan-oos <jordan@oos-studio.com>
2020-02-17 15:55:13 -0500

    Took other components out of branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
 import React from 'react'
+import ImageCenteredText from './components/ImageCenteredText'
+
 
 function App() {
+
+  const thirdStyles = {
+    container: {
+      margin: '5%',
+    },
+    image: {
+    },
+    overlay: {
+    },
+    text: {
+      color: 'white',
+      fontSize: 25,
+    }
+  }
+
+  const props = {
+    text: 'Welcome',
+    image: {
+      image: 'https://picsum.photos/1920/1080',
+      title: 'Picture'
+    },
+  }
+
   return (
-    <div className="App">
-      Hi
+
+    <div style={divStyles.container}>
+
+      <div style={divStyles.thirds}>
+        <ImageCenteredText styles={thirdStyles} {...props} />
+        <ImageCenteredText styles={thirdStyles} {...props} />
+        <ImageCenteredText styles={thirdStyles} {...props} />
+      </div>
+
     </div>
+
   )
+}
+
+const divStyles = {
+  container: {
+  },
+  thirds: {
+    margin: '5%',
+    display: 'flex',
+    flexDirection: 'row',
+    height: '100vh',
+  },
 }
 
 export default App
