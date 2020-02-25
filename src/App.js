7e0d3c5fd3fad735077f1ad7c8d222e40a7cbb52
jordan-oos <jordan@oos-studio.com>
2020-02-25 10:58:17 -0500

    Added default styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,71 @@
 import React from 'react'
 import { Footer } from './components'
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 function App() {
   const footerProps = {
-    title: 'oos',
-    description: `Pete is a loser and doesn't want to talk to us`
+    columns: [
+      {
+        heading: '',
+        type: 'image',
+        src: 'https://picsum.photos/200/200',
+        title: 'Image',
+      },
+      {
+      heading: 'Contact Us',
+      type: 'links',
+      links: [
+        {
+          title: 'Email | jordan@oos-studio.com',
+          url: '#/A'
+        },
+        {
+          title: "Phone | 919-600-4862",
+          url: "#/B"
+        },
+      ]
+    },
+      {
+        heading: 'Address',
+        type: 'text',
+        text: [
+            '815 Frost Rd \n #503 \n Streetsboro, OH 44241'
+        ]
+      },
+      {
+        heading: 'Contact Form',
+        render: () => {
+          return (
+          <Form style={styles.form}>
+            <FormGroup style={styles.formGroup}>
+              <Input type='email' placeholder='Enter your email address' />
+            </FormGroup>
+            <FormGroup style={styles.formGroup}>
+              <Input type='text' placeholder='Subject' />
+            </FormGroup>
+            <FormGroup style={styles.formGroup}>
+              <Input type='textarea' placeholder='Enter your message here' />
+            </FormGroup>
+          </Form>
+          )
+        }
+      },
+      ],
+  }
+
+  const styles = {
+    form: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+    },
+    formGroup: {
+      width: '100%',
+      margin: 5,
+    }
   }
   return (
-    <Footer />
+    <Footer columns={footerProps.columns}/>
   )
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React, { Component} from 'react'
-import { Container, Col, Row} from 'reactstrap'
+import { Container, Col, Row, Media} from 'reactstrap'
 import mergeStyle from '../utils/StyleMerge'
 
 class Footer extends Component {
@@ -15,8 +15,95 @@ class Footer extends Component {
     return columnStyle
   }
 
-  renderColumn = (column, index) => {
+  getHeaderStyle = index => {
+    const { styles } = this.props
+    let headerStyle = styles.defaultHeader
+    if (styles.columns[index]) {
+      if ('header' in styles.columns[index]) {
+        //headerStyle = mergeStyles(styles.defaultHeader, styles.columns[index].header)
+      }
+    }
+    return headerStyle
+  }
 
+  getLinkStyle = index => {
+    const { styles } = this.props
+    let linkStyle = styles.defaultLink
+    if (styles.columns[index]) {
+      if ('link' in styles.columns[index]) {
+        //linkStyle = mergeStyles(styles.defaultLink, styles.columns[index].link)
+      }
+    }
+    return linkStyle
+  }
+
+  getImageStyle = index => {
+    const { styles } = this.props
+    let imageStyle = styles.defaultImage
+    if (styles.columns[index]) {
+      if ('image' in styles.columns[index]) {
+        //imageStyle = mergeStyles(styles.defaultImage, styles.columns[index].image)
+      }
+    }
+    return imageStyle
+  }
+
+  getTextStyle = index => {
+    const { styles } = this.props
+    let textStyle = styles.defaultText
+    if(styles.columns[index]) {
+      if('text' in styles.columns[index]) {
+        //textStyle = mergeStyles(styles.defaultText, styles.columns[index].text)
+      }
+    }
+
+    return textStyle
+  }
+
+  renderHeader = (column, index) => {
+    const headerStyle = this.getHeaderStyle(index)
+    if(column.heading.length) {
+      return <div style={headerStyle}>{column.heading}</div>
+    } else {
+      return null
+    }
+  }
+
+  renderColumn = (column, index) => {
+    const linkStyle = this.getLinkStyle(index)
+    const imageStyle = this.getImageStyle(index)
+    const textStyle = this.getTextStyle(index)
+
+    switch(column.type) {
+      case 'text':
+        return (
+          <React.Fragment>
+            {this.renderHeader(column, index)}
+            {column.text.map(txt => <div style={textStyle}>{txt}</div>)}
+          </React.Fragment>
+        )
+      case 'links':
+        return (
+          <React.Fragment>
+            {this.renderHeader(column, index)}
+            {column.links.map(link => <a href={link.url}  style={linkStyle}>{link.title}</a>)}
+          </React.Fragment>
+        )
+      case 'image':
+        return (
+          <React.Fragment>
+            {this.renderHeader(column, index)}
+            <Media object src={column.src} alt={column.title} style={imageStyle}/>
+          </React.Fragment>
+        )
+      default:
+        return (
+          <React.Fragment>
+            {this.renderHeader(column, index)}
+            {column.render()}
+          </React.Fragment>
+        )
+    }
   }
 
   render() {
@@ -37,13 +124,45 @@ class Footer extends Component {
 }
 
 const defaultStyles = {
+  container: {
+    backgroundColor: 'tan',
+    width: '100vw',
+    display: 'flex',
+  },
   defaultColumn: {
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'relative',
+    width: '20%',
+    margin: '2%',
+    marginRight: '5%',
+    marginLeft: '5%',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  defaultHeader: {
+    position: 'absolute',
+    top: 0,
+    fontSize: 25,
+    marginBottom: '20%',
+    color: 'white',
+  },
+  defaultLink: {
+    fontSize: 18,
+    color: 'grey',
+  },
+  defaultText: {
+    fontSize: 18,
+    color: 'grey',
+  },
+  defaultImage: {
 
   },
+  columns: [],
 }
 
 Footer.defaultProps = {
-
+  columns: [],
 }
 
 
