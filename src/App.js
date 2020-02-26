77cfde202362da64cd225b134480dd4fccf3e406
chooper <colin@oos-studio.com>
2020-02-26 13:38:22 -0500

    Add FullScreenIntro to ProvidenceHomepage sample after merging master

--- src/App.js
+++ src/App.js
@@ -2,13 +2,7 @@ import React from 'react'
 import ProvidenceHomepage from "./samples/providence/ProvidenceHomepage"
 import './App.css'
 
-const fsIntroProps = { heightOffset: 20,
-  text: 'Welcome to the full screen intro page. This style is just a placeholder. Make it whatever you want!',
-  image: {
-    title: 'img',
-    url: 'https://picsum.photos/1000/700'
-  },
-}
+
 
 function App() {
   return <ProvidenceHomepage/>
