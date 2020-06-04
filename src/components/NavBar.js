7423af03ebb424fe6cef7c6d300f20cf2034a8d7
jordan-oos <jordan@oos-studio.com>
2020-06-04 16:18:19 -0400

    Fix nav issues

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -116,20 +116,20 @@ class NavBar extends Component {
                           <div style={megaMenuOpen ? deepmerge(styles.itemWrapper, styles.mmOpen.itemWrapper) : styles.itemWrapper}>
                               {items.map((item, index) => renderNavigationItems(item, index))}
                           </div>
+                          <div style={styles.megaMenu} onMouseLeave={() => hideMegaMenu()}>
+                              {aside !== null && aside !== undefined &&
+                              <React.Fragment>
+                                  <Media style={styles.asideImage} object src={aside.brand.image.src} alt={aside.brand.image.title}/>
+                                  <div style={styles.asideWrapper}>
+                                      <div style={styles.asideHeader}>{aside.header}</div>
+                                      <div style={styles.asideBody}>{aside.text}</div>
+                                  </div>
+                              </React.Fragment>
+                              }
+                              {megaMenu}
+                          </div>
                       </Nav>
                   </Collapse>
-                  <div style={styles.megaMenu} onMouseLeave={() => hideMegaMenu()}>
-                      {aside !== null && aside !== undefined &&
-                      <React.Fragment>
-                          <Media style={styles.asideImage} object src={aside.brand.image.src} alt={aside.brand.image.title}/>
-                          <div style={styles.asideWrapper}>
-                              <div style={styles.asideHeader}>{aside.header}</div>
-                              <div style={styles.asideBody}>{aside.text}</div>
-                          </div>
-                      </React.Fragment>
-                      }
-                      {megaMenu}
-                  </div>
               </Navbar>
           </div>
         )
