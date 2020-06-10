fb93836d1a61f232089d3290db02c5214475c385
jordan-oos <jordan@oos-studio.com>
2020-06-10 13:37:35 -0400

    Add NavLink to Button component.

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Button as BasicButton } from 'reactstrap'
 import mergeStyles from '../utils/StyleMerge'
 import deepmerge from 'deepmerge'
+import { NavLink } from './index'
 
 class Button extends Component {
   constructor(props) {
@@ -29,14 +30,16 @@ class Button extends Component {
   }
 
   render() {
-    const { styles, onClick } = this.props
+    const { styles, onClick, history, useRouter, item } = this.props
     const { hovered } = this.state
 
     const activeStyles = hovered ? deepmerge(styles, styles.hovered) : styles
 
     return (
       <BasicButton onClick={onClick} onMouseEnter={this.handleHover} onMouseLeave={this.handleHover} style={activeStyles}>
-        {this.props.children}
+        <NavLink useRouter={useRouter} histor={history} item={item}>
+          {this.props.children}
+        </NavLink>
       </BasicButton>
     )
   }
