a31808386fc77ddfc3e0f4ffc8444c5b60f09813
jordan-oos <jordan@oos-studio.com>
2020-06-23 11:27:05 -0400

    Fix NavLink call in Button component.

--- src/components/Button.js
+++ src/components/Button.js
@@ -37,7 +37,7 @@ class Button extends Component {
 
     return (
       <BasicButton onClick={onClick} onMouseEnter={this.handleHover} onMouseLeave={this.handleHover} style={activeStyles}>
-        {item ? (<NavLink useRouter={useRouter} histor={history} item={item} style={styles}>
+        {item ? (<NavLink useRouter={useRouter} history={history} item={item} style={styles}>
             {this.props.children}
           </NavLink>)
           :
