880c99d2e790bb7bd4378f21e869bea1cb296b38
jordan-oos <jordan@oos-studio.com>
2020-07-07 15:16:44 -0400

    Default style update for navlink

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -31,7 +31,7 @@ class NavLink extends Component {
     const { dropdown, onMouseEnter, onMouseLeave, style, item, children, useRouter } = this.props
     const { clickLink } = this
 
-   return dropdown ? (<UncontrolledDropdown style={style} ref={r => this._navLink = r} onMouseEnter={onMouseEnter} nav inNavbar onMouseLeave={onMouseLeave} onClick={clickLink}>{children}</UncontrolledDropdown>) : useRouter ? (<RSNavLink onClick={clickLink} href={item.url} style={style} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} >{children}</RSNavLink>) : (<a href={item.url} style={style} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>{children}</a>)
+   return dropdown ? (<UncontrolledDropdown style={{paddingLeft: 0, paddingRight: 0, ...style,}} ref={r => this._navLink = r} onMouseEnter={onMouseEnter} nav inNavbar onMouseLeave={onMouseLeave} onClick={clickLink}>{children}</UncontrolledDropdown>) : useRouter ? (<RSNavLink onClick={clickLink} href={item.url} style={{paddingLeft: 0, paddingRight: 0, ...style,}} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} >{children}</RSNavLink>) : (<a href={item.url} style={{paddingLeft: 0, paddingRight: 0, ...style,}} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>{children}</a>)
  }
 }
 
