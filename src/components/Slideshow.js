f99489f976946bb7a7fddcfda3337ced365180be
chooper <colin@oos-studio.com>
2020-07-30 13:47:06 -0400

    Add split text component and button to slideshow slides

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -3,29 +3,10 @@ import { Fade, Slide, Zoom } from 'react-slideshow-image'
 import mergeStyles from '../utils/StyleMerge'
 import withSizes from '../utils/Sizes'
 import deepmerge from 'deepmerge'
+import SplitWrappedLines from './SplitWrappedLines'
+import Radium from 'radium'
 
 class Slideshow extends Component {
-  state = {
-    titleLines: [],
-  }
-
-  splitWrappedLinesRef = React.createRef()
-
-  componentDidMount() {
-    window.addEventListener('resize', this.onResize)
-    this.onResize()
-  }
-
-  onResize = () => {
-    const lines = getLineBreaks(this.splitWrappedLinesRef.current.childNodes[0])
-
-    this.setState({
-      titleLines: lines
-    })
-
-    console.log(lines)
-  }
-
   renderSlide = () => {
     const { slides, options, styles, getStyle } = this.props
     const _styles = deepmerge(styles, slideStyles)
@@ -53,30 +34,32 @@ class Slideshow extends Component {
   }
   renderFade = () => {
     const { slides, options, styles, getStyle } = this.props
-    const { titleLines } = this.state
     const _styles = deepmerge(styles, fadeStyles)
 
     return (
       <Fade {...options} className="slideshowWrapper">
         {slides.map((slide, index) => {
+          console.log(slide)
           return(
-          <div key={index} style={_styles.slide}>
-            <div style={_styles.imageWrapper}>
-              {slide.source && <img src={slide.source} style={_styles.image} alt={`img${index}`}/>}
+          <div key={index} style={getStyle(_styles.slide)}>
+            <div style={getStyle(_styles.imageWrapper)}>
+              {slide.source && <img src={slide.source} style={getStyle(_styles.image)} alt={`img${index}`}/>}
             </div>
-            <div style={_styles.titleWrapper}>
+            <div style={getStyle(_styles.titleWrapper)}>
               {options.splitWrappedLines ?
-                <React.Fragment>
-                  <span ref={this.splitWrappedLinesRef} className={'split-wrapped-lines'} style={{...getStyle(styles.titleLine), position: 'absolute', opacity: 0}}>{slide.title}</span>
-                  {titleLines.map((line, subIndex) =>
-                      <span key={subIndex} style={getStyle(styles.titleLine)}>{line}</span>
-                  )}
-                </React.Fragment>
+                <SplitWrappedLines text={slide.title} styles={{ line: styles.titleLine }} />
                 : slide.title.map((title, subIndex) => {
                   return (
                     <span key={subIndex} style={getStyle(title.style)}>{title.text}</span>
                   )
               })}
+              {
+                slide.button ?
+                  <div style={getStyle(styles.buttonWrapper)}>
+                    <div onClick={slide.button.onClick} key={`button-${index}`} style={getStyle(styles.button)}>{slide.button.text}</div>
+                  </div>
+                  : null
+              }
             </div>
           </div>
           )
@@ -112,44 +95,6 @@ class Slideshow extends Component {
   }
 }
 
-function getLineBreaks(node) {
-  // we only deal with TextNodes
-  if(!node || !node.parentNode || node.nodeType !== 3)
-    return [];
-  // our Range object form which we'll get the characters positions
-  const range = document.createRange();
-  // here we'll store all our lines
-  const lines = [];
-  // begin at the first char
-  range.setStart(node, 0);
-  // initial position
-  let prevBottom = range.getBoundingClientRect().bottom;
-  let str = node.textContent;
-  let current = 1; // we already got index 0
-  let lastFound = 0;
-  let bottom = 0;
-  // iterate over all characters
-  while(current <= str.length) {
-    // move our cursor
-    range.setStart(node, current);
-    if(current < str.length -1)
-      range.setEnd(node, current+1);
-    bottom = range.getBoundingClientRect().bottom;
-    if(bottom > prevBottom) { // line break
-      lines.push(
-        str.substr(lastFound , current - lastFound) // text content
-      );
-      prevBottom = bottom;
-      lastFound = current;
-    }
-    current++;
-  }
-  // push the last line
-  lines.push(str.substr(lastFound));
-
-  return lines;
-}
-
 const fadeStyles = {
 
 }
@@ -159,6 +104,15 @@ const slideStyles = {
 }
 
 const defaultStyles = {
+  button: {
+    display: 'inline-block',
+    cursor: 'pointer',
+  },
+  buttonWrapper: {
+  },
+  titleLine: {
+
+  },
   container: {
     width: '100%',
     height: '100%',
@@ -190,4 +144,4 @@ const defaultStyles = {
   },
 }
 
-export default mergeStyles(defaultStyles)(withSizes(Slideshow))
+export default mergeStyles(defaultStyles)(withSizes(Radium(Slideshow)))
--- src/samples/oos/Homepage.js
+++ src/samples/oos/Homepage.js
@@ -4,18 +4,13 @@ import { Header } from '../../components/index'
 const scrollImg = 'https://s3.us-east-2.amazonaws.com/cdn.www.oos-studio.com/prod/scrollDownAnimation.png'
 
 class Homepage extends Component {
-  componentDidMount() {
-    window.addEventListener('resize', this.onResize)
-    this.onResize()
-  }
-
-  onResize = () => {
-
-  }
 
   render() {
     return (
+      <React.Fragment>
       <Header styles={styles} slideshow={slideshow} scrollImg={scrollImg}/>
+      <div style={{height: 500}}/>
+      </React.Fragment>
     )
   }
 }
@@ -32,25 +27,56 @@ const styles = {
     },
   },
   slideshow: {
+    image: {
+      position: 'fixed',
+      left: 0,
+      right: 0,
+    },
     titleLine: {
-      fontSize: 36,
+      fontSize: 35,
       lineHeight: 1.25,
-      marginTop: 5,
-      marginBottom: 5,
+      margin: 5,
       paddingRight: 10,
       paddingLeft: 10,
       display: 'inline-block',
       backdropFilter: 'blur(10px)',
       '-webkit-backdrop-filter': 'blur(10px)',
-      backgroundColor: 'rgba(255,255,255,0.2)',
-      borderRadius: 7.5,
+      backgroundColor: 'rgba(255,255,255,0.75)',
+      borderRadius: 8,
       textTransform: 'uppercase',
       textAlign: 'left',
       fontFamily: 'capitolina, serif',
       fontWeight: 700,
-      md: {
+      whiteSpace: 'nowrap',
+      lg: {
         fontSize: 32,
       },
+    },
+    button: {
+      backgroundColor: '#007589',
+      color: 'white',
+      borderRadius: 8,
+      margin: 5,
+      fontSize: 21,
+      paddingTop: 3,
+      paddingBottom: 5,
+      paddingLeft: 20,
+      paddingRight: 20,
+      transition: 'background-color 0.35s ease-out',
+      ':hover': {
+        backgroundColor: '#005363',
+      },
+    },
+    buttonWrapper: {
+      marginTop: 50,
+      sm: {
+        position: 'absolute',
+        bottom: 100,
+        left: 0,
+        right: 0,
+        textAlign: 'center',
+      },
+
     },
     container: {
       height: '100%',
@@ -62,15 +88,22 @@ const styles = {
       },
     },
     titleWrapper: {
-      width: '60%',
-      //backgroundColor: 'green',
+      width: '57.5%',
       left: 0,
-      color: 'red',
-      paddingTop: 100,
-      paddingLeft: '20%',
+      color: '#007589',
+      paddingTop: '25.5vh',
+      height: '100%',
+      paddingLeft: '25%',
+      paddingRight: 0,
       placeContent: 'start',
       display: 'block',
       textAlign: 'left',
+      sm: {
+        width: '100%',
+        paddingLeft: 15,
+        paddingRight: 15,
+        paddingTop: 100,
+      }
     },
   },
   scroll: {
@@ -90,7 +123,7 @@ const styles = {
 
 const slideshow = {
   options: {
-    duration: 5000,
+    duration: 7000,
     transitionDuration: 500,
     infinite: true,
     arrows: false,
@@ -99,9 +132,21 @@ const slideshow = {
   },
   slides: [
     {
-      source: 'https://s3.us-east-2.amazonaws.com/cdn.www.oos-studio.com/prod/DigitalTransformation_2.jpg',
+      source: 'http://storage.googleapis.com/test.cdn.oos-software.com/logos/TFL_App_MockupPhoto@3x.png',
       title: 'oos Builds Custom Apps That People Love to Use.',
+      button: {
+        text: 'Watch Video',
+
+      }
     },
+    {
+      source: 'https://s3.us-east-2.amazonaws.com/cdn.www.oos-studio.com/prod/DigitalTransformation_2.jpg',
+      title: 'oos Solutions Make Smart Manufacturing Simple.',
+      button: {
+        text: 'Watch Video',
+        onClick: () => console.log('clicked de button')
+      }
+    }
   ]
 }
 
