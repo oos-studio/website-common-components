a0e4a415955cf1de0e4b6cd9104ed5eaf0883e2c
jordan-oos <jordan@oos-studio.com>
2020-08-19 12:47:36 -0400

    NavLink checks if the app is being run from a RN WebView and if so, sends the url to the app rather than push to the router history.

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -4,42 +4,86 @@ import { NavLink as RSNavLink, UncontrolledDropdown } from 'reactstrap'
 class NavLink extends Component {
   _navLink = React.createRef()
   clickLink = (e) => {
-    const { useRouter, onClickItem, item, history, hideDropDown, append, isNativeApp, webViewRef } = this.props
+    const {
+      useRouter,
+      onClickItem,
+      item,
+      history,
+      hideDropDown,
+      append,
+    } = this.props
 
     e.stopPropagation()
 
-    if(isNativeApp) {
-      if (webViewRef) {
-        webViewRef.postMessage(item.url)
+    if (window.isNativeApp) {
+      if (window.ReactNativeWebView) {
+        e.preventDefault()
+        window.ReactNativeWebView.postMessage(item.url)
       }
       return
     }
 
-    if(hideDropDown !== undefined) {
+    if (hideDropDown !== undefined) {
       hideDropDown()
     }
 
-    if(useRouter && history) {
+    if (useRouter && history) {
       e.preventDefault()
       let url
-      if(item.url) {
+      if (item.url) {
         url = item.url[0] === '/' || !append ? item.url : `/${item.url}`
         history.push(url)
       }
     }
-    if(onClickItem) {
+    if (onClickItem) {
       onClickItem(e, item.url)
     }
   }
   toggle = () => {
     this._navLink.toggle()
   }
- render() {
-    const { dropdown, onMouseEnter, onMouseLeave, style, item, children, useRouter } = this.props
+  render() {
+    const {
+      dropdown,
+      onMouseEnter,
+      onMouseLeave,
+      style,
+      item,
+      children,
+      useRouter,
+    } = this.props
     const { clickLink } = this
 
-   return dropdown ? (<UncontrolledDropdown style={{paddingLeft: 0, paddingRight: 0, ...style,}} ref={r => this._navLink = r} onMouseEnter={onMouseEnter} nav inNavbar onMouseLeave={onMouseLeave} onClick={clickLink}>{children}</UncontrolledDropdown>) : useRouter ? (<RSNavLink onClick={clickLink} href={item.url} style={{paddingLeft: 0, paddingRight: 0, ...style,}} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} >{children}</RSNavLink>) : (<a href={item.url} style={{paddingLeft: 0, paddingRight: 0, ...style,}} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>{children}</a>)
- }
+    return dropdown ? (
+      <UncontrolledDropdown
+        style={{ paddingLeft: 0, paddingRight: 0, ...style }}
+        ref={(r) => (this._navLink = r)}
+        onMouseEnter={onMouseEnter}
+        nav
+        inNavbar
+        onMouseLeave={onMouseLeave}
+        onClick={clickLink}>
+        {children}
+      </UncontrolledDropdown>
+    ) : useRouter ? (
+      <RSNavLink
+        onClick={clickLink}
+        href={item.url}
+        style={{ paddingLeft: 0, paddingRight: 0, ...style }}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}>
+        {children}
+      </RSNavLink>
+    ) : (
+      <a
+        href={item.url}
+        style={{ paddingLeft: 0, paddingRight: 0, ...style }}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}>
+        {children}
+      </a>
+    )
+  }
 }
 
 NavLink.defaultProps = {
